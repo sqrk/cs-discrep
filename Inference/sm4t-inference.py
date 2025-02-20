b526568af3e4b5c7abfd9bb09de310ae1351c088
Karima Kadaoui <kariima.kad@gmail.com>
2025-02-20 16:21:37 +0400

    Add sm4t inference script

---
+++ Inference/sm4t-inference.py
@@ -0,0 +1,53 @@
+from transformers import SeamlessM4TProcessor, SeamlessM4Tv2Model
+from datasets import load_dataset, Audio
+import numpy as np
+import torch, random
+
+dataset_name = '' # provide dataset name/path
+cache_dir = '' # provide path to your preferred cache directory
+reference_label = '' # depending on the dataset, provide the name used for ground truth (e.g. 'sentence', 'label', 'text')
+target_language = '' # target lang is REQUIRED because sm4t has translation capabilities and has eng as tgt lang by def. Find the ids here https://huggingface.co/facebook/seamless-m4t-v2-large#supported-languages
+source_language = '' # leave empty if no LID, otherwise provide language
+test_split = '' # provide the name of the split you're doing inference on (e.g. test)
+output_file_path = f'../Predictions/{dataset_name}-sm4t-{source_language or "noLID"}.tsv' # if there's a slash in your dataset_name make sure to slice e.g. for dataset_name 'sqrk/dataset', use dataset_name[5:] in your output file name
+model_name = 'facebook/seamless-m4t-v2-large'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch_dtype = torch.float16 if device.type == 'cuda' else torch.float32
+batch_size = 16
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+
+print(f'DATASET: {dataset_name}\nMODEL: {model_name}\nLANGUAGE: {source_language or "noLID"}\nSEED: {seed}')
+
+
+dataset = load_dataset(dataset_name, cache_dir=cache_dir)
+dataset = dataset.cast_column("audio", Audio(sampling_rate=16000))
+dataset[test_split] = dataset[test_split].shuffle(seed=seed)
+
+model = SeamlessM4Tv2Model.from_pretrained(model_name, device_map=device, cache_dir=cache_dir)
+if source_language:
+    processor = SeamlessM4TProcessor.from_pretrained(model_name, src_lang=source_language, cache_dir=cache_dir)
+else:
+    processor = SeamlessM4TProcessor.from_pretrained(model_name, cache_dir=cache_dir)
+
+def transcribe(batch):
+    file_names = [audio['path'] for audio in batch['audio']]
+    audio_arrays = [np.array(audio["array"], dtype=np.float32) for audio in batch["audio"]]
+    audio_inputs = processor(audios=audio_arrays, return_tensors="pt", sampling_rate=16000).to(device)
+    output_tokens = model.generate(**audio_inputs, tgt_lang=target_language, generate_speech=False) 
+    results = [processor.decode(item, skip_special_tokens=True) for item in output_tokens[0].detach().cpu().tolist()]
+    return {"prediction": results,
+            "file_name": file_names}
+
+dataset[test_split] = dataset[test_split].map(transcribe, batched=True, batch_size=batch_size)
+
+with open(output_file_path, 'w') as fout:
+    fout.write('file_name\treference\tprediction\n')
+    for item in dataset[test_split]:
+        fout.write(f"{item['file_name']}\t{item[reference_label]}\t{item['prediction']}\n")
+
+print("Inference complete. Predictions saved.")
